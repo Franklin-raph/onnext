ee3cdf7b4324a8bf88a6893679e1bb715dc2e408
Franklin-raph <igboekwulusifranklin@gmail.com>
2021-07-12 11:15:10 +0100

    included the scroll spy

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ $(window).scroll(function() {
         });
         $('.navbar').css('box-shadow','0px 1px 1px rgb(32, 32, 32, 0.3)');
         $('#back2Top').fadeIn();
+        
     } else {
         $('#nav-bg').removeClass('fixed-top bg-nav');
         $('.nav-item a').css('color','#fff')
@@ -22,4 +23,19 @@ $(window).scroll(function() {
         $('.navbar').css('box-shadow','0px 0px 0px #000000');
         $('#back2Top').fadeOut();
     }
+
+    if(height > 0){
+        $('#home').css({
+            'background-color':'#fff',
+            'color': 'rgb(0, 162, 255)',
+            'border-radius': '50px 50px 0 50px;'
+        })
+    }else{
+        $('#home').css({
+            'background-color':'rgb(0, 162, 255)',
+            'color': '#fff',
+            'border-radius': '50px 50px 0 50px;'
+        })
+    }
 });
+
